bebc1965e1d5c3220b0c7aaf33722e85061d4c85
jessejohn01 <jesse.arstein@yahoo.com>
2020-09-24 01:43:04 -0600

    Completed HW2

--- examples/scratch.sql
+++ examples/scratch.sql
@@ -1,2 +1,5 @@
-SELECT * FROM artists
-WHERE ArtistId=10;
+SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName
+FROM tracks
+JOIN albums on tracks.AlbumId = albums.AlbumId
+JOIN artists on albums.ArtistId = artists.ArtistId
+WHERE tracks.Milliseconds >  1000 * 60 * 6
