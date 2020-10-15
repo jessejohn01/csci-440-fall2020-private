d5be7b555ba28a84a6ef494bc6279b3ae7df53b4
jessejohn01 <jesse.arstein@yahoo.com>
2020-10-15 17:33:44 -0600

    Resolve scratch.sql merge errors.

--- examples/scratch.sql
+++
@@ -1,5 +0,0 @@
-SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName
-FROM tracks
-JOIN albums on tracks.AlbumId = albums.AlbumId
-JOIN artists on albums.ArtistId = artists.ArtistId
-WHERE tracks.Milliseconds >  1000 * 60 * 6
